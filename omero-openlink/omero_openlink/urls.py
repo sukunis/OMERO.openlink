17afa6fe074e3c5700672d36153f161ba0eb4a99
Michele Bortolomeazzi <michele.bortolomeazzi@gmail.com>
2024-02-12 12:01:55 +0100

    Replace deprecated django function url with re_path (since django 3.1) (#5)
    * replace url with re_path
    
    * Replaces url function also in comment.

--- omero-openlink/omero_openlink/urls.py
+++ omero-openlink/omero_openlink/urls.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from django.conf.urls import url
+from django.urls import re_path 
 from . import views
 
 
@@ -8,12 +8,12 @@ urlpatterns = [
 
 
     # index 'home page' of the openlink app
-    url(r'^$', views.openlink, name='openlink_index'),
+    re_path(r'^$', views.openlink, name='openlink_index'),
 
-    url(r'^deleteOpenLink/?',views.delete, name='openlink-delete'),
+    re_path(r'^deleteOpenLink/?',views.delete, name='openlink-delete'),
 
     #show debugoutput: replace in url "webclient" by "omero_openlink/debugoutput"
-    #url(r'^debugoutput/$',views.debugoutput,name='debugoutput'),
+    #re_path(r'^debugoutput/$',views.debugoutput,name='debugoutput'),
 
 
 ]
