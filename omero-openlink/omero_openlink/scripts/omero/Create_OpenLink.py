7a499e9def7938b419e106b28414ed61ace6e86b
Susanne Kunis <sukunis@uos.de>
2024-07-30 11:01:04 +0200

    add documentations

--- omero-openlink/omero_openlink/scripts/omero/Create_OpenLink.py
+++ omero-openlink/omero_openlink/scripts/omero/Create_OpenLink.py
@@ -7,9 +7,8 @@ This script creates a download slot with links to the given image objects. Direc
 on the omero project-dataset-image structure.
 @author Susanne Kunis
 <a href="mailto:sinukesus@gmail.com">sinukesus@gmail.com"</a>
-@version 0.1.0
-
 """
+
 import os
 import sys
 import random
@@ -82,10 +81,17 @@ MAX_PATHLENGTH = 200 # max pathlength in windows:256
 NOTIFICATION_LIST={}
 
 def get_realpath(path):
-    # return target of symlink
+    """return target of symlink"""
     return Path(path).resolve().as_posix()
 
 def get_omero_paths(client):
+    """
+    Args:
+        client: calling omero client object
+    Returns:
+        managed_repo_dir: path to MANAGED_REPOSITORY of this omero instance (see config: omero.managed.dir)
+        orig_repo_dir: path to data of this omero instance (see config: omero.data.dir)
+    """
     resources = client.sf.sharedResources()
     repos = resources.repositories()
     managed_repo_dir = None
@@ -128,6 +134,13 @@ def get_omero_paths(client):
 
 # returning file paths in qiven directory and all subdirs
 def get_file_paths(directory, file_paths):
+    """Append absolute paths of file in qiven directory and in all subdirs
+    Args:
+        directory:
+        file_paths: list of absolute paths of files
+    Returns:
+        file_paths: list of absolute paths of files
+    """
     for f in glob.iglob("%s/*"%directory,recursive=True):
         if os.path.isdir(f):
             file_paths=get_file_paths(f, file_paths)
@@ -139,6 +152,12 @@ def get_file_paths(directory, file_paths):
 
 
 def addToCurlFile(base,hashName):
+    """
+    Args:
+        base: absolute path to openlink area
+        hashName: name of openlink area dir
+    """
+
     curlFile = os.path.join(base,CURL_FILE)
     contentFile = os.path.join(base,CONTENT_FILE)
     fileList = get_file_paths(base,[])
@@ -167,6 +186,13 @@ def addToCurlFile(base,hashName):
 # get location of sources in managed rep
 # TODO: filenames with special characters makes problems
 def getOriginalFile(imageObj):
+    """Get location of sources in managed rep.
+    Args:
+        imageObj: image object
+    Returns:
+        path: path to file
+        name: name of the file
+    """
     fileset = imageObj.getFileset()
 
     for origFile in fileset.listFiles():
@@ -218,11 +244,19 @@ def addToDictContent(path,id):
     CONTENT_DICT.update({path:id})
 
 def createObjectDir(type,ppath,object,name):
-    """ Create directory of <name> in <ppath> and add this information with object id to dict content.
-    Normally <name> is the name of the given object.
-    If the directory still exists, but was created from a different object, append object id to the name of the dir.
+    """
+      Create new directory of <name> in <ppath> and add this path and the object id to global dict CONTENT_DICT .
+      Normally <name> is the name of the given object.
+      If the directory still exists, but was created from a different object, append object id to the name of the dir.
 
-    RETURN: absolute path of created dir"""
+      Args:
+          ppath: path to directory where the new directory should be created
+          object: omero object
+          name: name of the new directory
+      RETURN:
+          absolute path of created dir
+
+    """
 
     path=os.path.join(ppath,name)
     if not os.path.exists(path):
@@ -266,6 +300,15 @@ def getPath(image, slot):
 
 
 def userIsOwner(conn,userName, id):
+    """
+    Check if owner of the image == calling user.
+    Args:
+        conn: BlitzGateway object
+        userName: name of the user
+        id: image Id
+    Returns:
+        True if user is onwer, else false
+    """
     image = conn.getObject('Image',id)
     if image.getOwnerOmeName()==userName:
         return True
@@ -274,10 +317,12 @@ def userIsOwner(conn,userName, id):
 
 def isAllowedToShareData(conn,userID):
     '''
-    Test if user is owner of the current group and the group permission is not private
-    :param conn:
-    :param userID:
-    :return:
+    Return true if given user is owner of the current group and the group permission is NOT private.
+
+    Args:
+        conn: BlitzGateway connection
+        userID: user ID
+
     '''
     group = conn.getGroupFromContext()
     print("# Current group: %s"%group.getName())
@@ -304,6 +349,14 @@ def isAllowedToShareData(conn,userID):
 
 
 def getFilesetPath(conn,id):
+    """
+    Return path to the fileset of given image
+
+    Args:
+        conn: BlitzGateway connection
+        id: image id
+
+    """
     image = conn.getObject('Image',id)
     # this will include pre-FS data IF images were archived on import
     # specifically count Fileset files
@@ -360,6 +413,12 @@ def checkLinks(src,targetDir,name,linkNames, linkTarget, id):
 
 
 def createSymlinks(linkNames, linkTarget):
+    """
+    Create symlink on the system if not exists.
+    Args:
+        linkNames: name of symlink
+        linkTarget: target where the link points to
+    """
     if linkNames is not None and len(linkNames)>0:
         for (src, dest) in zip(linkTarget,linkNames):
             #print("# create link: %s ->\n\t%s"%(dest,src))
@@ -369,6 +428,12 @@ def createSymlinks(linkNames, linkTarget):
                 print("# skip:: Link still exists: ",src)
 
 def addAttachment(obj,tdir):
+    """
+    Args:
+        obj: object with attachments
+        tdir: path where links to the attachments should be created
+    Returns:
+    """
     global ORIGINAL_REP
     if tdir is not None:
         for ann in obj.listAnnotations():
@@ -388,6 +453,12 @@ def addAttachment(obj,tdir):
 
 
 def addToNotifyList(user, image_ID):
+    """
+    Validate user mail and add image id as well email to list of user that get a notification mail.
+    Args:
+        user: user object
+        image_ID: image id
+    """
     userID=user.getId() # Initialises also the proxy object for simpleMarshal
     dic = user.simpleMarshal()
     if 'email' in dic and dic['email']:
@@ -416,6 +487,17 @@ def get_owner_of_data(conn, image):
 
 
 def addImages(conn,slot,images,user,addAttachments, allowedToShare,targetDir=None):
+    """
+    Check if parent dir (dataset) exists (and create one if not) and afterwards calls createObjectDir for given image objects
+    Args:
+        conn: BlitzGateway connection
+        slot: path to access area
+        images: List of OMERO dataset objects
+        user: user object
+        addAttachments (bool):
+        allowedToShare (bool):
+        targetDir: parent dataset dir if exists
+    """
     linkNames=[]
     linkTarget=[]
     userName = user.getName()
@@ -466,6 +548,19 @@ def addImages(conn,slot,images,user,addAttachments, allowedToShare,targetDir=Non
 
 
 def addDatasets(conn,slot, datasets,user,addAttachments,allowedToShare,targetDir=None):
+    """
+    TODO: doubled with addPath?
+    Check if parent project dir exists (and create one if not) and afterwards calls createObjectDir for given dataset objects
+    and add images
+    Args:
+      conn: BlitzGateway connection
+      slot: path to access area
+      projects: List of OMERO dataset objects
+      user: user object
+      addAttachments (bool):
+      allowedToShare (bool):
+      targetDir: parent project dir if exists
+    """
     for dataset in datasets:
         # check if parent project dir still exists
         if not targetDir:
@@ -490,6 +585,17 @@ def addDatasets(conn,slot, datasets,user,addAttachments,allowedToShare,targetDir
 
 
 def addProjects(conn,slot,projects,user,addAttachments,allowedToShare):
+    """
+    TODO: doubled with addPath?
+    Calls createObjectDir for given project objects and add child datasets
+    Args:
+        conn: BlitzGateway connection
+        slot: path to access area
+        projects: List of OMERO project objects
+        user: user object
+        addAttachments (bool):
+        allowedToShare (bool):
+    """
     for project in projects:
         linkDir=createObjectDir("P",slot,project,project.getName())
         if linkDir is not None:
@@ -499,12 +605,25 @@ def addProjects(conn,slot,projects,user,addAttachments,allowedToShare):
             addDatasets(conn,slot,project.listChildren(),user,addAttachments,allowedToShare,linkDir)
 
 def getRandomString(n):
-    # generating random strings
+    """generating random strings
+    Args:
+        n: length of random number
+    """
     res = ''.join(random.choices(string.ascii_uppercase +
                                  string.digits, k = n))
     return res
 
 def generateHashName(user,n,accessAreaName):
+    """
+    Generate name for access area like "rn_<randomNumber>_<userID>_<userSpecificAreaName>"
+    Args:
+        user: user object
+        n: lenght of random number
+        accessAreaName: access area name specified by user
+    Returns:
+        hashname: string like "rn_<randomNumber>_<userID>_<userSpecificAreaName>"
+    """
+
     hashName = "%s_%s_%s_%s"%("rn",getRandomString(n),user.getId(),accessAreaName)
     return hashName
 
@@ -516,6 +635,15 @@ def createAreaName():
     return timeStr
 
 def generateNewAccessArea(user,accessAreaName):
+    """
+    Args:
+        user: user object
+        accessAreaName: access area name specified by user
+    Returns:
+        accessAreaPath: path to area
+        hashName: whole name of area directory
+    """
+
     hashName = generateHashName(user,LENGTH_HASH,accessAreaName)
 
     while os.path.exists(os.path.join(OPENLINK_DIR,hashName)):
@@ -531,10 +659,11 @@ def email_results(conn, imageIDs,email,smtpObj):
     """
     E-mail the result to the user.
 
-    @param conn:    The BlitzGateway connection
-    @param results: Dict of (imageId,text_result) pairs
-    @param report:  The results report
-    @param params:  The script parameters
+    Args:
+        conn:    The BlitzGateway connection
+        imageIDs: ID's of data that was shared
+        email: email address of receiver
+        smtpObj:
     """
     sharerName=conn.getUser().getName()
     msg = MIMEMultipart()
@@ -554,8 +683,7 @@ def email_results(conn, imageIDs,email,smtpObj):
 
 def notifyMembers(conn):
     '''
-    Notify owner of the data if they was shared by group owner
-    :return:
+    Notify owner of the data via mail if they was shared by group owner
     '''
     global NOTIFICATION_LIST
 
@@ -575,11 +703,14 @@ def notifyMembers(conn):
 def addObjToAccessArea(conn,params,availableSlots=None,paths=None):
     """
     add selected object and its content to a slot on OPENLINK_DIR as link to sources on ManagedRepository
-    :param paths:
-    :param availableSlots:
-    :param conn:
-    :param params:
-    :return:
+
+    Args:
+        conn: current user connection
+        params: user input
+        availableSlots: list of available slots for current user
+        paths: list of paths to the available slots of the surrent user
+    Returns:
+        message:
     """
 
     # init Notifationlist
@@ -646,6 +777,9 @@ def addObjToAccessArea(conn,params,availableSlots=None,paths=None):
 
 
 def parseAccessAreaNames(p):
+    """
+    Return name of openLink area specified by user
+    """
     try:
         name = re.search(GET_SLOTNAME_PATTERN, p).group(1)
     except AttributeError:
@@ -655,6 +789,12 @@ def parseAccessAreaNames(p):
 
 
 def getAreasOfUser(id):
+    """
+    Args:
+        id: user id
+    Returns:
+        values: list of paths that contains ID in the folder name
+    """
     values = []
     p="%s/%s%s_*"%(OPENLINK_DIR,OPENLINK_PATTERN,id)
     values = glob.glob(p)
@@ -666,8 +806,12 @@ def getAreasOfUser(id):
 def getAvailableSlots(conn):
     """
     get all slots for current user (OPENLINK_DIR/rn_<RN>_<userid>_<accessAreaName>/;
-    :param conn:
-    :return:
+    Args:
+        conn: connection of calling user
+    Returns:
+        values : list of description of slots; one slot is described like "<areaName> [created <date>]"; if no slot
+                    available it returns ['No OpenLinks available for <userName>]
+        paths : list of path to slots
     """
     values = None
     try:
